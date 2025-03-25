fba5d7ec3146b616db26a49eaba9fa1f460b8149
Gabriel <gabrodriguez153@gmail.com>
2025-03-25 20:36:54 -0300

    feat: starting the variations of screens for many number of electors

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Cabeca(){
   return(
-    <footer className="bg-black border-gray-200 dark:bg-gray-900 mt-[100px] h-[94px] flex justify-center">
+    <footer className="bg-black border-gray-200 dark:bg-gray-900 mt-[130px] h-[85px] flex justify-center">
       <img src="/logo_sp.png" alt="são paulo" className="w-[210px]" />
     </footer>
   );
--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -3,11 +3,17 @@ import React from "react";
 export default function Conteudo() {
   return(
     <div className="w-[50rem] h-[30rem] bg-white ml-67 mt-8">
-      <h1 className="font-(family-name:--font-roboto-slab) text-left mb-2" style={{ color: "#1A6C7C", fontSize: "36px", fontWeight: "500" }}>Vitrine - Representantes 5°DSM</h1>
+      <h1 className="font-(family-name:--font-roboto-slab) text-left mb-2" style={{ color: "#1A6C7C", fontSize: "36px", fontWeight: "500" }}>Vitrine - Representantes | 5°DSM</h1>
       <div className="w-[80rem] h-[1px]" style={{ backgroundColor: "#666666"}} ></div>
-      <div className="mt-8 ml-10 w-[75rem] relative">
+      <div className="mt-8 ml-40 w-[1000px] relative flex">
+        <div className="w-[500px] h-30">
         <img src="/candi1.png" alt="candi" />
         <h4 className="font-(family-name:--font-roboto-slab) mt-[-70px] text-center" style={{ color: "#fff", fontSize: "34px"}}>Nome do Aluno</h4>
+        </div>
+        <div className="w-[500px]">
+        <img src="/candi2.png" alt="candi" />
+        <h4 className="font-(family-name:--font-roboto-slab) mt-[-70px] text-center" style={{ color: "#fff", fontSize: "34px"}}>Nome do Aluno</h4>
+        </div>
       </div>
     </div>
   );
