e8e699256f0e2be5a23ffdb8c2576e415c115f4f
Gabriel <gabrodriguez153@gmail.com>
2025-03-28 15:57:54 -0300

    fix: ajust little details in code

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -9,24 +9,26 @@ interface ImgsProps {
 
 function Imgs({ src, alt, nome }: ImgsProps) {
   return (
-    <div className="relative h-[500px] items-center overflow-hidden max-h-[300px] md:max-h-[400px]">
-      <Image
-        src={src}
-        alt={alt}
-        fill
-        className="object-cover md:rounded-lg  "
-      />
-      <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute md:rounded-lg bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black to-transparent" />
+     
+      <div className="relative h-[500px] max-h-[300px] md:max-h-[400px] ">
+        <Image src={src} alt={alt} fill className="object-cover" />
+        <div className="absolute inset-0 pointer-events-none">
+          <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-black to-transparent" />
+        </div>
+        <div className="absolute bottom-4 w-full text-center">
+          <h4
+            className="font-(family-name:--font-roboto-slab) mt-[-70px] text-center"
+            style={{
+              color: "#fff",
+              fontSize: "30px",
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+            }}
+          >
+            {nome}
+          </h4>
+        </div>
       </div>
-      <div className="absolute bottom-4 w-full text-center">
-        <h4 
-          className="font-(family-name:--font-roboto-slab) mt-[-70px] text-center" style={{ color: "#fff", fontSize: "30px", textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)"}}
-        >
-          {nome}
-        </h4>
-      </div>
-    </div>
+
   );
 }
 
--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -2,12 +2,11 @@ import React, { JSX } from "react";
 import Imgs from "./container";
 
 export default function Conteudo(): JSX.Element {
-
   const candidatos = [
     { src: "/candi1.jpg", alt: "Candidato 1", nome: "Nome Aluno 1" },
     { src: "/candi2.jpg", alt: "Candidato 2", nome: "Nome Aluno 2" },
-   { src: "/candi3.jpg", alt: "Candidato 3", nome: "Nome Aluno 3" },
-   { src: "/candi4.jpg", alt: "Candidato 4", nome: "Nome Aluno 4" },
+    { src: "/candi3.jpg", alt: "Candidato 3", nome: "Nome Aluno 3" },
+    { src: "/candi4.jpg", alt: "Candidato 4", nome: "Nome Aluno 4" },
   ];
 
   let widthClass = "w-full";
@@ -19,26 +18,23 @@ export default function Conteudo(): JSX.Element {
     widthClass = "w-full md:w-1/2";
   }
 
-
-
   return (
     <div className="bg-white mx-auto mt-8 max-w-5xl p-4">
-     <h1
-        className="text-left mb-2 font-(family-name:--font-roboto-slab)" style={{ color: "#1A6C7C", fontSize: "32px", fontWeight: "500" }}
+      <h1
+        className="text-left mb-2 font-(family-name:--font-roboto-slab)"
+        style={{ color: "#1A6C7C", fontSize: "32px", fontWeight: "500" }}
       >
         Vitrine - Representantes | 5°DSM
       </h1>
 
       <div className="w-full h-px bg-gray-500 mb-4"></div>
-      <div className="flex flex-wrap mb-20">
-      {candidatos.map((cand, index) => (
-         <div key={index} className={`${widthClass} `}>
-         
-           <Imgs src={cand.src} alt={cand.alt} nome={cand.nome} />
-
-       </div>
+      <div className="flex flex-wrap mb-20 rounded-xl overflow-hidden">
+        {candidatos.map((cand, index) => (
+          <div key={index} className={`${widthClass} `}>
+            <Imgs src={cand.src} alt={cand.alt} nome={cand.nome} />
+          </div>
         ))}
       </div>
-      </div>
+    </div>
   );
 }
