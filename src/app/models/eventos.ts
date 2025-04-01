e81196641a622482633a73c40380f7019ace1746
Yasmin Pires <160610423+yaspires@users.noreply.github.com>
2025-04-01 10:50:18 -0300

    feat: added TypeScript models

---
+++ src/app/models/eventos.ts
@@ -0,0 +1,12 @@
+export interface Evento {
+    id: number;
+    nome: string;
+    tipo_evento: string;
+    data_inicio: Date;
+    data_fim: Date;
+    descricao: string;
+    status: string;
+    data_alteracao: Date;
+    data_criacao: Date;
+    curso_semestre: string;
+  }
