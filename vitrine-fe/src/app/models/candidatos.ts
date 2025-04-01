4f0e0af52a4a325e94268c71ba20832d1d6c0748
Yasmin Pires <160610423+yaspires@users.noreply.github.com>
2025-04-01 10:42:52 -0300

    feat: Criação das models no front-end

---
+++ vitrine-fe/src/app/models/candidatos.ts
@@ -0,0 +1,9 @@
+export interface Candidato {
+    id: number;
+    id_aluno?: number;
+    id_projeto?: number;
+    id_evento?: number;
+    situacao_candidato: string;
+    qrcode: string;
+  }
+  
