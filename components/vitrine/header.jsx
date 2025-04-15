31d1cd4aa0da7a8c4515662ee52a8982a188199d
DSM5 <fatec-dsm5@academico.intra>
2025-04-14 22:28:42 -0300

    feat: finalizando header

--- components/vitrine/header.jsx
+++ components/vitrine/header.jsx
@@ -7,7 +7,7 @@ export default function SpHeader() {
   return (
     <header className="w-full">
       <div className=" flex flex-col">
-        <div className="w-[776px] h-[80px] flex items-center bg-black rounded-r-[100px]">
+        <div className="max-w-[1000px] min-h-[80px] flex items-center bg-black rounded-r-[100px]">
           <div className="header-container-4k h-full relative w-[248px] flex align-center ml-8">
             <Image
               src="/logo_sp.svg"
